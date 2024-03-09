c7016b691c7c6101de8d6e354150520b9dbd82e3
Your Name <you@example.com>
2024-03-09 16:03:13 +0300

    feat: init

---
+++ index.js
@@ -0,0 +1,20 @@
+const fastify = require('fastify')({
+  logger: true,
+});
+const path = require('node:path');
+
+fastify.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/', // optional: default '/'
+  constraints: { host: 'example.com' }, // optional: default {}
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
